0143e7be854bd6cf8c2964c683ef6cc312ecc86d
Aditya Kumar <contact.adibitz@gmail.com>
2026-08-22 13:14:18 +0530

    updated

---
+++ .changeset/README.md
@@ -0,0 +1,5 @@
+# Changesets
+
+Hello and welcome! This folder has been setup by `@changesets/cli`.
+
+For more information, see the [Changesets documentation](https://github.com/changesets/changesets).
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-<div>
-  <h1 className="text-amber-300 text-3xl text-center animate-">Welcome to our dynasty!
-  </h1>
-
-</div>
-  );
+  return <div>Landing page</div>;
 }
